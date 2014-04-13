559884682a1590ace320d4d75280af5e8c8c938c
Andrico <Andrico@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-13 16:22:06 +0000

    Update Free Plugin
    git-svn-id: https://plugins.svn.wordpress.org/advanced-widgets/tags/1.1.0@892315 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ aw_ajustes.php
@@ -0,0 +1,74 @@
+<style>
+.advanced-widgets input.ok{
+	border-color: green;
+}
+.advanced-widgets input.error{
+	border-color: red;
+}
+</style>
+<div class="wrap advanced-widgets">
+	<h2><?php _e("Hello!",'advanced-widgets');?></h2>
+	<?php
+	if($aw_salvado){
+		?>
+		<div id="message" class="updated">
+			<p><?php _e("Saved!",'advanced-widgets');?></p>
+		</div>
+		<?php
+	}
+	?>
+	<h3>Advanced Widgets</h3>
+	<p><?php _e('Add widgets in your sidebars and then choose where they will be shown! It has never been so easy to personalize the widgets section!','advanced-widgets');?></p>
+	<h4><?php _e('How to use','advanced-widgets');?></h4>
+	<ol>
+		<li><?php _e('Install the plugin','advanced-widgets');?></li>
+		<li><?php _e('Activate the plugin','advanced-widgets');?></li>
+		<li><?php _e('Add Widgets on the sidebars in the Appearance->widgets section','advanced-widgets');?></li>
+		<li><?php _e('Configurate the Widget by pressing on the bar of the title and then on the Configuration button','advanced-widgets');?></li>
+		<li><?php _e('Configurate the options and add filters to show these widgets in a personalized way','advanced-widgets');?></li>
+	</ol>
+	
+	<h4><?php _e('Help!','advanced-widgets');?></h4>
+	<p><?php _e('For any requirement, contact me to:','advanced-widgets');?> Nicolás Guglielmi <a href="mailto:nicolas.guglielmi@gmail.com">nicolas.guglielmi@gmail.com</a></p>
+	
+	<h4><?php _e('Activation','advanced-widgets');?></h4>
+	<table>
+		<tr>
+			<td>
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<input type="hidden" name="hosted_button_id" value="C6BWB77KBG664">
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+			</td>
+			<td>
+				<p>
+					<!--<?php _e('After using it for 60 days, you will have a delay of a second per each day oy use. To avoid having this delay, you must send your e-mail, the Paypal Id donation and the url of the site found below the following post box','advanced-widgets');?><br/>-->
+					Nicolás Guglielmi <a href="mailto:nicolas.guglielmi@gmail.com">nicolas.guglielmi@gmail.com</a>
+				</p>
+			</td>
+		</tr>
+	</table>
+	
+
+	<form action="" method="post">
+		<input type="hidden" name="aw_action" value="save" />
+		
+		<p>
+			<label>
+				<?php _e('Activation code','advanced-widgets');?><br/>
+				<input size="50" class="<?php echo ($aw_check_code)?"ok":"error";?>" type="text" name="aw_code" value="<?php echo $aw_code;?>" />
+			</label>
+		</p>
+		<p>
+			<?php _e('Site url','advanced-widgets');?><br/>
+			<b><?php echo site_url();?></b>
+		</p>
+		<p>
+			<button class="button-primary"><?php _e("Save",'advanced-widgets');?></button>
+		</p>
+	</form>
+
+
+</div>
